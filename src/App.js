7297da2eec32afb09351bf127340940b087fed19
FatemehNilchi <fatemehnilchi77@gmail.com>
2023-03-31 23:49:12 +0430

    mini project landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import './App.css'
+import Article from './Article/Article'
+import Header from './Header/Header'
 
 export default function App() {
   return (
     <div>
-      <div className='nav-background'>
-        <div className='nav-container'>
-          <header id='nav-header'>
-            <h1><a href='#' id='nav-logo'>Fatemeh Nilchi</a></h1>
-          </header>
-          <nav>
-            <ul className='nav-ul'>
-              <li><a href='#' className='nav-link'>Home</a></li>
-              <li><a href='#' className='nav-link'>News</a></li>
-              <li><a href='#' className='nav-link'>Contact</a></li>
-              <li><a href='#' className='nav-link'>About</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <Header/>
+      <Article/>
     </div>
   )
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+import NavBar from './NavBar'
+export default function Header(){
+    return (
+        <div>
+            <section>
+                <span>Welcome</span>
+            </section>
+            <NavBar/>
+        </div>
+    )
+}
