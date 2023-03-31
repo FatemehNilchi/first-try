aef23f3c286f3c961d30c700ab3c150dc878b761
FatemehNilchi <fatemehnilchi77@gmail.com>
2023-03-31 22:20:44 +0430

    mini project nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react'
+import './App.css'
 
-import Footer from './Footer/Footer'
-import Header from './Header/Header'
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Header></Header>
-        <h1>body</h1>
-    <Footer></Footer>
+    <div>
+      <div className='nav-background'>
+        <div className='nav-container'>
+          <header id='nav-header'>
+            <h1><a href='#' id='nav-logo'>Fatemeh Nilchi</a></h1>
+          </header>
+          <nav>
+            <ul className='nav-ul'>
+              <li><a href='#' className='nav-link'>Home</a></li>
+              <li><a href='#' className='nav-link'>News</a></li>
+              <li><a href='#' className='nav-link'>Contact</a></li>
+              <li><a href='#' className='nav-link'>About</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
-    
-    );
+  )
 }
 
-export default App;
+
